78f6a9e0d10e0db134e27cecc620d7795367a26a
Pavel Efimenko <efimenkopavel1985@gmail.com>
2020-07-17 07:09:04 +0300

    refactor unit test

--- tests/RealEstateApi.UnitTests/PropertyServiceTests.cs
+++ tests/RealEstateApi.UnitTests/PropertyServiceTests.cs
@@ -14,28 +14,39 @@ namespace RealEstateApi.UnitTests
 {
     public class PropertyServiceTests
     {
+        private const string ApiKey = "api-key";
+        private const string SiteUrl = "http://www.zillow.com";
+        private const string City = "Union";
+        private const string State = "NJ";
+        private const string Address = "2277 Copper Hill Dr";
+
         [Fact]
         public async Task GetSoldProperties_Returns_Results()
         {
             // Arrange
+            var sut = await CreateSut();
+
+            // Act
+            var properties = await sut.GetSoldProperties(new GetSoldPropertiesRequest { Address = Address, City = City, Country = State, PageSize = 10 });
+
+            // Assert
+            Assert.Single(properties);
+            Assert.Equal(40083537, properties.Single().Zpid);
+        }
+
+        private async Task<PropertyService> CreateSut()
+        {
             var content = File.ReadAllText("example_response_success.xml");
             var serializerSettings = new XmlContentSerializerSettings { XmlNamespaces = new XmlSerializerNamespaces() };
             var xmlContentSerializer = new XmlContentSerializer(serializerSettings);
             var dto = await xmlContentSerializer.DeserializeAsync<SearchResults>(new StringContent(content));
             var mockZillowApi = new Mock<IZillowApi>();
-            mockZillowApi.Setup(api => api.GetDeepSearchResults(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+            mockZillowApi.Setup(api => api.GetDeepSearchResults(It.IsAny<string>(), $"{City}, {State}", Address))
                 .ReturnsAsync(dto);
-            var settings = Options.Create(new ZillowApiSettings { ApiKey = "ApiKey", SiteUrl = "http://test.com" });
-            var sut = new PropertyService(mockZillowApi.Object, settings);
 
-            // Act
-            var result = await sut.GetSoldProperties(new GetSoldPropertiesRequest());
+            var settings = Options.Create(new ZillowApiSettings { ApiKey = ApiKey, SiteUrl = SiteUrl });
 
-            // Assert
-            var properties = dto.Response.Results.Properties;
-            Assert.NotNull(properties);
-            Assert.Single(properties);
-            Assert.Equal(40083537, properties.Single().Zpid);
+            return new PropertyService(mockZillowApi.Object, settings);
         }
     }
 }
